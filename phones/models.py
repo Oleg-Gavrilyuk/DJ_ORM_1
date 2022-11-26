48a4a4ec0e4cca5b0245773670c2c98fbecc10cf
Oleg-Gavrilyuk <gavrilyuk161090@yandex.ru>
2022-11-26 14:23:43 +0300

    ORM1

---
+++ phones/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Phone(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
